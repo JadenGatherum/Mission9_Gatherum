47be9832001e89fd766efd50b5df35aeabd04bde
JadenGatherum <jadenbb@byu.edu>
2024-03-06 21:39:53 -0700

    Mission 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
+// Importing the React library, which is necessary for creating React components
 import React from 'react';
-import logo from './logo.svg';
+
+// Importing CSS styles for the App component
 import './App.css';
 
-function App() {
+// Importing JSON data from a file named CollegeBasketballTeams.json
+import jsonData from './CollegeBasketballTeams.json';
+
+// Declaring a constant variable named teams and assigning it the type of TeamProps array,
+// and initializing it with the 'teams' array from the imported JSON data
+const teams: TeamProps[] = jsonData.teams;
+
+// Defining the structure of each team object using an interface named TeamProps
+interface TeamProps {
+  tid: number; // Team ID
+  cid: number; // Conference ID
+  did: number; // Division ID
+  school: string; // School name
+  name: string; // Team name
+  abbrev: string; // Team abbreviation
+  pop: number; // Population
+  city: string; // City
+  state: string; // State
+  latitude: number; // Latitude
+  longitude: number; // Longitude
+}
+
+// Functional component to display a welcome message
+function Welcome() {
+  return <h1>College Basketball Teams</h1>;
+}
+
+// Class-based component to render individual team details
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h4>{oneTeam.name}</h4>
+        <h4>{oneTeam.state}</h4>
+      </div>
+    );
+  }
+}
+
+// Functional component to render a list of teams
+function TeamList() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Mapping over the 'teams' array and rendering a Team component for each team object */}
+      {teams.map((team) => (
+        <Team {...team} key={team.tid} />
+      ))}
     </div>
   );
 }
 
+// Main App component
+function App() {
+  return (
+    <div className="App">
+      {/* Rendering the Welcome component */}
+      <Welcome />
+      {/* Rendering the TeamList component to display the list of teams */}
+      <TeamList />
+    </div>
+  );
+}
+
+// Exporting the App component as the default export
 export default App;
